fe568d5d42c0ad328c69fc01bc564422d287d304
Kayla Atsila Ivanka <79374942+kaylaatsila@users.noreply.github.com>
2021-09-20 21:39:47 +0700

    update main2

--- latihan-uml/src/Main2.java
+++ latihan-uml/src/Main2.java
@@ -1,5 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
-
+        Employee[] e = new Employee[2];
+        SalariedEmployee se = new SalariedEmployee();
+        HourlyEmployee he = new HourlyEmployee();
     }
 }
