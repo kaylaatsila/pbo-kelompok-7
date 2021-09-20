922340418b694ddeb72a98cfe7231ab26559a42d
muhaymi nurdin <82698265+muhaymi@users.noreply.github.com>
2021-09-20 21:43:41 +0700

    add setter getter

--- latihan-uml/src/HourlyEmployee.java
+++ latihan-uml/src/HourlyEmployee.java
@@ -2,5 +2,22 @@ class HourlyEmployee extends Employee{
     private int hoursWorked;
     private int hourlyPayment;
     
+       public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hw) {
+        hoursWorked = hw;
+    }
+
+    public int getHourlyPayment() {
+        return hourlyPayment;
+    }
+
+    public void setHourlyPayment(int hp) {
+        hourlyPayment = hp;
+    }
+
+    
     
 }
